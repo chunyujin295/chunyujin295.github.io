1346b28e44929adc240ce275bbf6704abdabfb17
chunyujin <3029795434@qq.com>
2023-01-20 00:41:47 +0800

    Site updated: 2023-01-20 00:41:47

--- js/fps.js
+++ js/fps.js
@@ -2,7 +2,7 @@
  * @Author: chunyujin 3029795434@qq.com
  * @Date: 2023-01-19 23:24:47
  * @LastEditors: chunyujin 3029795434@qq.com
- * @LastEditTime: 2023-01-19 23:24:51
+ * @LastEditTime: 2023-01-20 00:28:02
  * @FilePath: /MyWeb/themes/butterfly/source/js/fps.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */

3557a63f1fee53afd400a204bfe5e7d1ffa256dd
chunyujin <3029795434@qq.com>
2023-03-09 13:57:06 +0800

    Site updated: 2023-03-09 13:57:06

--- js/title 2.js
+++
@@ -1,1 +0,0 @@
-var titleTime,OriginTitile=document.title;document.addEventListener("visibilitychange",(function(){document.hidden?(document.title="👀跑哪里去了",clearTimeout(titleTime)):(document.title="欢迎回来😋",titleTime=setTimeout((function(){document.title=OriginTitile}),2e3))}));

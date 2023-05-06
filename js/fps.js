03ca66526dafeb8ee11e31b5131a3f296b4fa491
chunyujin <3029795434@qq.com>
2023-05-06 19:36:40 +0800

    Site updated: 2023-05-06 19:36:40

--- js/fps.js
+++ js/fps.js
@@ -1,1 +1,59 @@
-if(null==window.localStorage.getItem("fpson")||"1"==window.localStorage.getItem("fpson")){var rAF=window.requestAnimationFrame||window.webkitRequestAnimationFrame||function(e){window.setTimeout(e,1e3/60)},frame=0,allFrameCount=0,lastTime=Date.now(),lastFameTime=Date.now(),loop=function(){var e=Date.now(),a=e-lastFameTime,o=Math.round(1e3/a);if(lastFameTime=e,allFrameCount++,frame++,e>1e3+lastTime){if((o=Math.round(1e3*frame/(e-lastTime)))<=5)var n='<span style="color:#bd0000">卡成ppt🤢</span>';else if(o<=15)n='<span style="color:red">电竞级帧率😖</span>';else if(o<=25)n='<span style="color:orange">有点难受😨</span>';else if(o<35)n='<span style="color:#9338e6">不太流畅🙄</span>';else if(o<=45)n='<span style="color:#08b7e4">还不错哦😁</span>';else n='<span style="color:#39c5bb">十分流畅🤣</span>';document.getElementById("fps").innerHTML=`FPS:${o} ${n}`,frame=0,lastTime=e}rAF(loop)};loop()}else document.getElementById("fps").style="display:none!important";
+/*
+ * @Author: chunyujin 3029795434@qq.com
+ * @Date: 2023-01-19 23:24:47
+ * @LastEditors: chunyujin 3029795434@qq.com
+ * @LastEditTime: 2023-01-20 00:28:02
+ * @FilePath: /MyWeb/themes/butterfly/source/js/fps.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+if (window.localStorage.getItem("fpson") == undefined || window.localStorage.getItem("fpson") == "1") {
+    var rAF = function () {
+        return (
+            window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            function (callback) {
+                window.setTimeout(callback, 1000 / 60);
+            }
+        );
+    }();
+    var frame = 0;
+    var allFrameCount = 0;
+    var lastTime = Date.now();
+    var lastFameTime = Date.now();
+    var loop = function () {
+        var now = Date.now();
+        var fs = (now - lastFameTime);
+        var fps = Math.round(1000 / fs);
+
+        lastFameTime = now;
+        // 不置 0，在动画的开头及结尾记录此值的差值算出 FPS
+        allFrameCount++;
+        frame++;
+
+        if (now > 1000 + lastTime) {
+            var fps = Math.round((frame * 1000) / (now - lastTime));
+            if (fps <= 5) {
+                var kd = `<span style="color:#bd0000">卡成ppt🤢</span>`
+            } else if (fps <= 15) {
+                var kd = `<span style="color:red">电竞级帧率😖</span>`
+            } else if (fps <= 25) {
+                var kd = `<span style="color:orange">有点难受😨</span>`
+            } else if (fps < 35) {
+                var kd = `<span style="color:#9338e6">不太流畅🙄</span>`
+            } else if (fps <= 45) {
+                var kd = `<span style="color:#08b7e4">还不错哦😁</span>`
+            } else {
+                var kd = `<span style="color:#39c5bb">十分流畅🤣</span>`
+            }
+            document.getElementById("fps").innerHTML = `FPS:${fps} ${kd}`;
+            frame = 0;
+            lastTime = now;
+        };
+
+        rAF(loop);
+    }
+
+    loop();
+} else {
+    document.getElementById("fps").style = "display:none!important"
+}

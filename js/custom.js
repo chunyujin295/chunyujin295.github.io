c3a41c5cfbbbbb67579e2d886ebc07e79eca1059
chenyujin-admin <3029795434@qq.com>
2024-10-22 16:00:26 +0800

    Site updated: 2024-10-22 16:00:24

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,1 @@
-/*
- * @Author: chunyujin 3029795434@qq.com
- * @Date: 2022-12-31 10:57:28
- * @LastEditors: chunyujin 3029795434@qq.com
- * @LastEditTime: 2022-12-31 10:57:35
- * @FilePath: /MyWeb/themes/butterfly/source/js/custom.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-document.onkeydown = function (e) {
-    if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) return btf.snackbarShow("你真坏，不能打开控制台喔!"), event.keyCode = 0, event.returnValue = !1, !1
-};
+document.onkeydown=function(e){if(123==e.keyCode||e.ctrlKey&&e.shiftKey&&(74===e.keyCode||73===e.keyCode||67===e.keyCode)||e.ctrlKey&&85===e.keyCode)return btf.snackbarShow("你真坏，不能打开控制台喔!"),event.keyCode=0,event.returnValue=!1,!1};
